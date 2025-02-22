75291ee8a0d5c4a92e446d38aa044608783b151a
Schwaips <charlie.bertrand@live.com>
2025-02-22 12:23:28 +0100

    rubocoped

--- app/helpers/svg_helper.rb
+++ app/helpers/svg_helper.rb
@@ -2,9 +2,9 @@
 
 module SvgHelper
   def svg_tag(icon_name, options = {})
-    file = Rails.root.join('app', 'assets', 'images', 'icons', "#{icon_name}.svg").read
+    file = Rails.root.join("app", "assets", "images", "icons", "#{icon_name}.svg").read
     doc = Nokogiri::HTML::DocumentFragment.parse file
-    svg = doc.at_css 'svg'
+    svg = doc.at_css "svg"
 
     options.each { |attr, value| svg[attr.to_s] = value }
 
